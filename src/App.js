229704cd6937dd1d68adbb6be2f3e8a53b4e2133
Pedro Valente <valentedev09@gmail.com>
2022-02-10 18:16:16 -0300

    tmdb integrado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import react from 'react'
+import React, { useEffect } from 'react'
+import Tmdb from './tmdb'
 
 export default () => {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      console.log(list);
+    }
+
+    loadAll()
+  }, [])
+
   return (
     <div>
       Hello World!!!
--- src/tmdb.js
+++ src/tmdb.js
@@ -1,2 +1,47 @@
 const API_KEY = '1014f9c3274eeca20f86d6d55490a756'
 const API_BASE = 'https://api.themoviedb.org/3'
+
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`)
+    const json = await req.json()
+    return json
+}
+
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'Originals',
+                title: 'Netflix originals',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Trending',
+                title: 'Trending',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Toprated',
+                title: 'Top rated',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Action',
+                title: 'Action',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Adventure',
+                title: 'Adventure',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            }
+        ]
+    }
+}
